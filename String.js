b9d55b7f71674f31b14e96e597a7375ba73d121f
Ankit Yadav <59208025+xxANKYxx@users.noreply.github.com>
2023-01-05 22:27:07 +0530

    Create String.js
    Solution of removing duplicate number from given array in javascript

---
+++ String.js
@@ -0,0 +1,11 @@
+// Remove Duplicates from Sorted Array
+
+var removeDuplicates = function(nums) {
+    let j=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] != nums[i+1]){
+            nums[j++] = nums[i];
+        }
+    };
+    return j; 
+}
